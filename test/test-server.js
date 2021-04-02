d18c573f0ea79036219dfb9101398f3bf9c54b43
kenneth <afolabi.kenneth@gmail.com>
2021-04-02 17:45:39 +0000

    test

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>This is Kenneth's App</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>This is Another Kenneth's App</h1>"));
         return done();
       });
 
